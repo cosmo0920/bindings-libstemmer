ef293b28452f194c80694bb54d22ac4418a29066
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-11-12 21:29:28 +0900

    split function

--- Bindings/Stemmer.hs
+++ Bindings/Stemmer.hs
@@ -4,6 +4,7 @@ import Bindings.Stemmer.Raw
 import Foreign.C.String
 import System.IO.Unsafe (unsafePerformIO)
 import Data.Char (toLower)
+import Foreign.Ptr
 
 data Encoding = UTF_8
               | ISO_8859_1
@@ -33,10 +34,19 @@ data Stem = Stem { language :: Language
                  , encoding :: Encoding }
           deriving Show
 
-init_stem :: Language -> Encoding -> IO Stem
-init_stem lang enc = do
+type Stemmer = Ptr C'sb_stemmer
+
+init_stemmer :: Language -> Encoding -> IO Stem
+init_stemmer lang enc = do
   return Stem { language = lang
               , encoding = enc  }
+
+new_stemmer :: Stem -> IO Stemmer
+new_stemmer Stem{..} = do
+  cword_enc <- encodingCString encoding
+  algorithm <- languageCString language
+  stemmer <- c'sb_stemmer_new algorithm cword_enc
+  return stemmer
 
 -- stem words with Stem Type and word
 -- * algorithm: Language
@@ -44,12 +54,13 @@ init_stem lang enc = do
 stemword :: Stem -> String -> IO String
 stemword Stem{..} word = do
   cword <- newCString word
-  cword_enc <- encodingCString encoding
-  algorithm <- languageCString language
-  stemmer <- c'sb_stemmer_new algorithm cword_enc
+  stemmer <- new_stemmer Stem{..}
   strPtr <- c'sb_stemmer_stem stemmer cword (fromIntegral $ length word)
   str_length <- c'sb_stemmer_length stemmer
   peekCStringLen (strPtr, fromIntegral str_length)
+
+delete_stemmer :: Stemmer -> IO ()
+delete_stemmer = c'sb_stemmer_delete
 
 -- stem words with unsafePerformIO
 unsafeStemword :: Stem -> String -> String
