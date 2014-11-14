6843c2e12ef3700f68d02f16daf5394cd815c40e
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-11-15 00:06:52 +0900

    remove explicit type annotation

--- Bindings/Stemmer/Simple.hs
+++ Bindings/Stemmer/Simple.hs
@@ -12,7 +12,7 @@ import Control.Monad.Trans.Class (lift)
 --  new & delete 'Stemmer' a.k.a (Ptr C'sb_stemmer) automatically.
 simple_stem :: StemConfig -> String -> IO String
 simple_stem StemConfig{..} word = runResourceT $ do
-  (key,stemmer) <- allocate ((new_stemmer StemConfig{..}) :: IO Stemmer) delete_stemmer
+  (key, stemmer) <- allocate (new_stemmer StemConfig{..}) delete_stemmer
   str <- lift $ stemword stemmer word
   lift $ release key
   return str
